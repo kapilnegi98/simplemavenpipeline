6c1f12755b5cf313f67c3ca0a06b311f4017adbc
Kapil Negi <kapil.negi@nagarro.com>
2020-03-21 17:39:41 +0530

    added Simple Maven Piepeline

---
+++ src/main/java/com/codekapil/App.java
@@ -0,0 +1,13 @@
+package com.codekapil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
